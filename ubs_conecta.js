6d996b64fed92ef4e9794fd1c43652a691f5c42f
Miguel Oliveira <201830677710@estudante.rn.gov.br>
2026-08-18 09:23:07 -0300

    Sistema web - Projeto UBS Conecta

---
+++ ubs_conecta.js
@@ -0,0 +1,85 @@
+function home(home) {
+    window.location.href = home;
+}
+
+function unidades(unidades) {
+    window.location.href = unidades;
+}
+function duvidas(duvidas) {
+    window.location.href = duvidas;
+}
+function contatos(contatos) {
+    window.location.href = contatos;
+}
+function ficha(ficha) {
+    window.location.href = ficha;
+}
+
+function pow() {
+    window.alert('Comentário sendo enviado...');
+}
+function pou() {
+    window.alert('Apagando comentário...');
+}
+
+
+
+function FONTES_1() {
+    window.location.href = ("https://maps.app.goo.gl/v5VqZmrCPnqXUX6aA");
+}
+function lagoa() {
+    window.location.href = ("https://maps.app.goo.gl/NSR9eFQfqF66ZPxo9");
+}
+function olho() {
+    window.location.href = ("https://maps.app.goo.gl/x51nDtPUBECCTGsm9");
+} function pajussara() {
+    window.location.href = ("https://maps.app.goo.gl/LpWEuuQYYt2MpE7U9");
+}
+function pitombeira() {
+    window.location.href = ("https://maps.app.goo.gl/kd1GrYcSU8sQY2YS6");
+} function retiro() {
+    window.location.href = ("https://maps.app.goo.gl/4XAHuMiJGjDJ1YYx9");
+} function riacho() {
+    window.location.href = ("https://maps.app.goo.gl/GBnn19fuwqFSGGBt9");
+} function timbauba() {
+    window.location.href = ("https://maps.app.goo.gl/zzxF9ywmy9XQjDpf9");
+} function farmacia() {
+    window.location.href = ("https://maps.app.goo.gl/UubvSQTgxpgrD7618");
+} function hospital() {
+    window.location.href = ("https://maps.app.goo.gl/1sP99AiJHLoCSo199");
+} function novo() {
+    window.location.href = ("https://maps.app.goo.gl/afvquNgKSPxQ5eJBA");
+} function lagoa_do_mato() {
+    window.location.href = ("https://maps.app.goo.gl/kTfZzrmZzG4KDZkZ6");
+} function santa() {
+    window.location.href = ("https://maps.app.goo.gl/NJdUPiFZuuRzjJ9d6");
+} function bairro() {
+    window.location.href = ("https://maps.app.goo.gl/NnsHB2s6xJzuVynX9");
+} function bairro_das_flores() {
+    window.location.href = ("https://maps.app.goo.gl/kwhQbt5Dv9PTgvk89");
+} function comum() {
+    window.location.href = ("https://maps.app.goo.gl/T13nWSo3bKfxNtWA7");
+} function fontes() {
+    window.location.href = ("https://maps.app.goo.gl/VG6eTEWcdEqGrEwv9");
+} function professor() {
+    window.location.href = ("https://maps.app.goo.gl/ErrrXVGTx43HtiLE9");
+} function quirambu() {
+    window.location.href = ("https://maps.app.goo.gl/efPnyk88dhKA4KTB6");
+} function sobrado() {
+    window.location.href = ("https://maps.app.goo.gl/wZC4vkqcPyeVCtR97");
+}
+
+function fazer() {
+    window.location.href = ("ficha.html")
+}
+
+
+function en() {
+    window.alert("Enviando sua ficha...");
+}
+function lixo() {
+    window.alert("Apagando tudo...");
+}
+function aj() {
+    window.alert("Preencha todos os campos abaixo corretamente e clique em enviar para completar seu cadastro. O seu cadastro será analisado pela UBS escolhida e você será informado em breve pelo seu e-mail e/ou telefone que você adicionou no cadastro.");
+}
